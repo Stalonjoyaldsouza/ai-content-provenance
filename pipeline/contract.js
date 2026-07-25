c236d1af984e12b0eb20f493305abb04dd1cb0f0
Stalonjoyaldsouza <stalonjdsouza24@gmail.com>
2026-07-25 23:15:39 +0530

    abi and contract.js to commut with contract implemented

---
+++ pipeline/contract.js
@@ -0,0 +1,31 @@
+import  {ethers} from "ethers";
+import {readFileSync} from "fs";
+import "dotenv/config"
+
+const abi =JSON.parse(readFileSync(new URL("./abi/ClaimRegistry.json",import.meta.url)));
+const provider = new ethers.JsonRpcSigner(process.env.SEPOLIA_RPC_URL);
+const wallet = new ethers.Wallet(process.env.provider);
+const contract = new ethers.Contract(process.env.CONTRACT_ADDRESS,abi.wallet);
+
+async function registerClaimOnChain(claimHash, ipfsCID, modelId){
+    const tx = await contract.registerClaim(claimHash, ipfsCID, modelId);
+    console.log("transaction sent",tx.hash);
+    
+    const receipt = await tx.wait();
+    console.log("confirmed in block :",receipt.blockNumber);
+
+    return  receipt 
+}
+
+async function getClaimFromChain(claimHash){
+    const record = await contract.getClaim(claimHash);
+    return {
+        claimHash: record.claimHash,
+        ipfsCID: record.ipfsCID,
+        modelId: record.modelId,
+        timestamp: Number(record.timestamp),
+        submitter: record.submitter
+  };
+}
+
+export {registerClaimOnChain,getClaimFromChain,contract}
