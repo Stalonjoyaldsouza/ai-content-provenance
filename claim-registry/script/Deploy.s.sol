8c7b1498c59b0353e5c3f42a624fef2d0fc17eba
Stalonjoyaldsouza <stalonjdsouza24@gmail.com>
2026-07-29 22:06:37 +0530

    contract is deployed on Sepolia Ethereum test network

--- claim-registry/script/Deploy.s.sol
+++ claim-registry/script/Deploy.s.sol
@@ -1,15 +1,15 @@
 pragma solidity ^0.8.24;
 
 import {Script, console} from "forge-std/Script.sol";
-import {ClaimRegistry} from "../src/ClaimRegistry.sol";
+import {BatchClaimRegistry} from "../src/BatchClaimRegistry.sol";
 
 contract DeployScript is Script {
-    function run() external returns (ClaimRegistry) {
+    function run() external returns (BatchClaimRegistry) {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
         vm.startBroadcast(deployerPrivateKey);
 
-        ClaimRegistry registry = new ClaimRegistry();
+        BatchClaimRegistry registry = new BatchClaimRegistry();
 
         vm.stopBroadcast();
 
--- claim-registry/src/BatchClaimRegistry.sol
+++ claim-registry/src/BatchClaimRegistry.sol
@@ -1,7 +1,28 @@
 
 pragma solidity ^0.8.24;
 
-import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+library MerkleProof {
+    function verify(
+        bytes32[] memory proof,
+        bytes32 root,
+        bytes32 leaf
+    ) internal pure returns (bool) {
+        return processProof(proof, leaf) == root;
+    }
+
+    function processProof(bytes32[] memory proof, bytes32 leaf) internal pure returns (bytes32) {
+        bytes32 computedHash = leaf;
+        for (uint256 i = 0; i < proof.length; i++) {
+            bytes32 proofElement = proof[i];
+            if (computedHash <= proofElement) {
+                computedHash = keccak256(abi.encodePacked(computedHash, proofElement));
+            } else {
+                computedHash = keccak256(abi.encodePacked(proofElement, computedHash));
+            }
+        }
+        return computedHash;
+    }
+}
 
 contract BatchClaimRegistry {
     struct Batch {
