22d648dafe499569f8d331f0557a00575642652b
Stalonjoyaldsouza <stalonjdsouza24@gmail.com>
2026-08-17 01:12:23 +0530

    feat:event scanner to retrieve claims through cid

--- pipeline/indexer.js
+++ pipeline/indexer.js
@@ -11,8 +11,65 @@ async function scanEvents(contractInstance,filter,deploymentBlock) {
 
     for(let from = deploymentBlock ; from <=currentBlock ; from += chunk_size){
         const to = Math.min(deploymentBlock,from + chunk_size - 1);
-        const events = await contractInstance.queryfilter(filter,from ,to);
+        const events = await contractInstance.queryFilter(filter,from ,to);
         allevents.push(...events.filter((e)=>"args" in e));
     }
     return allevents;
+}
+
+export async function buildindex ({singleDeployBlock, batchDeployBlock}){
+    const singleEvent =await  scanEvents(
+        claimcontract, 
+        claimcontract.filters.ClaimRegistered,
+        singleDeployBlock
+    );
+    const batchEvent =await  scanEvents(
+        BatchContract,
+        BatchContract.filters.BatchRegistered,
+        batchDeployBlock
+    );
+     const record =[];
+
+    for(const e of singleEvent){
+        const cid = e.args.ipfsCID;
+        console.log('cidss:',cid);
+        try{
+            const content = await fetchClaimRecord(cid);
+            record.push({
+                mode: "single",
+                cid,
+                txId: e.transactionHash,
+                hash: e.args.claimHash,
+                modelId: e.args.modelId,
+                submitter: e.args.submitter,
+                ...content
+            });
+
+        }
+        catch(err){
+            console.log(`Failed to fetch ${cid}:`, err.message)
+
+        }
+    }
+    for(const e of batchEvent){
+        const Cid = e.args.ipfsCID;
+        console.log('cidss:',Cid);
+        try{
+            const content =await fetchClaimRecord(Cid);
+            record.push({
+                mode: "batch",
+                Cid,
+                txId: e.transactionHash,
+                hash: e.args.claimHash,
+                batchId: Number(e.args.batchId),
+                ...content
+            });
+
+        }
+        catch(err){
+            console.log(`Failed to fetch ${Cid}:`, err.message)
+
+        }
+    }
+
 }
