83671664f445ddba52012f187fe4a65a0da9029a
Stalonjoyaldsouza <stalonjdsouza24@gmail.com>
2026-08-18 23:16:45 +0530

    test:test output records stored  into json file

--- pipeline/tests/run-index.js
+++ pipeline/tests/run-index.js
@@ -1,9 +1,12 @@
 
 import { buildindex } from "../indexer.js";
+import {writeFileSync} from "fs"
 
 const records = await buildindex({
   singleDeployBlock: 11503148, 
   batchDeployBlock: 11503150
-})
-console.log(`Indexed ${records.length} claims total`);
+});
+writeFileSync("./index.json",JSON.stringify(records,null,2));
+
+console.log(`wrote ${records.length} records into json`);
 console.log(records[0]);
